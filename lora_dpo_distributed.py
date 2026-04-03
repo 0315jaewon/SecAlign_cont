bacf2a8e21f36ea7bd719e5e03d79c931ba0aa50
Jaewon Chang <changjaewon0315@berkeley.edu>
2026-04-03 13:27:28 -0700

    attacker norm logging

--- lora_dpo_distributed.py
+++ lora_dpo_distributed.py
@@ -900,6 +900,23 @@ class LoRADPORecipeDistributed(FTRecipeInterface):
             self._clear_defender_grads()
 
             self._attacker_optimizer.step()
+
+            with torch.no_grad():
+                attack_rows = self._attack_embedding_param[self._attack_token_ids]
+                attack_rows_norm = attack_rows.norm().detach()
+                attack_delta_norm = (
+                    attack_rows - self._initial_attack_embedding_rows
+                ).norm().detach()
+
+            utils.log_rank_zero(
+                log,
+                "attacker_inner_loop "
+                f"global_step={self.global_step} "
+                f"inner_step={_ + 1}/{self._attack_inner_steps} "
+                f"loss={attack_loss.detach().item():.6f} "
+                f"rows_norm={attack_rows_norm.item():.6f} "
+                f"delta_norm={attack_delta_norm.item():.6f}",
+            )
             last_metrics = attack_metrics
 
         if last_metrics is None:
